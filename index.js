ca86cae1ffe31286f004be163f42dd5f641d68c5
Nathan Parmar <nathparmar@icloud.com>
2023-11-28 16:57:09 +0000

    first part of MVP completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello");
+
+
+
+
+const getCountryByName = async (countryName) => {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    const data = await response.json()
+    document.querySelector("#get-country-by-name").innerText = data[0].name.common;
+    console.log(data);
+    
+}
+
+
+
+
+
+const button = document.querySelector("#btn1");
+button.addEventListener("click", () => { 
+    getCountryByName("Macau");
+    console.log("button clicked");
+});
+
+
