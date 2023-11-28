c7832d3c6a906adfd2b3912322d0678d78366de5
Nathan Parmar <nathparmar@icloud.com>
2023-11-28 20:44:34 +0000

    MVP almost finished, managed to get capital, population to display but not lnaguages array

--- index.js
+++ index.js
@@ -5,18 +5,28 @@
 const getCountryByName = async (countryName) => {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     const data = await response.json()
-    document.querySelector("#get-country-by-name").innerText = data[0].name.common;
-    console.log(data);
     
+    
+    document.querySelector("#get-country-by-name").innerText = data[0].name.common;
+    document.querySelector("#capital").innerText = data[0].capital;
+    // document.querySelector("#language").innerText = data[0].languages;
+    document.querySelector("#population").innerText = data[0].population;
+
+    // const languagesElement = document.querySelector("#language");
+    // const languagesString = data.map(language => language);
+    //     languagesElement.innerText = languagesString;
+    // const languages = data[0].languages;
+    // Object.keys(languages).forEach(languageCode) => (){
+    //     document.querySelector("#language").innerText = languages[languageCode];
+    // }
+   
 }
-
-
 
 
 
 const button = document.querySelector("#btn1");
 button.addEventListener("click", () => { 
-    getCountryByName("Macau");
+    getCountryByName("Portugal");
     console.log("button clicked");
 });
 
