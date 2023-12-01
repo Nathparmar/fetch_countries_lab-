0e5d69deb8cb1251d059ff4ef6653c6b2a1a7452
Nathan Parmar <nathparmar@icloud.com>
2023-12-01 14:54:50 +0000

    MVP completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 
 
+
+
 const getCountryByName = async (countryName) => {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     const data = await response.json()
@@ -9,25 +11,20 @@ const getCountryByName = async (countryName) => {
     
     document.querySelector("#get-country-by-name").innerText = data[0].name.common;
     document.querySelector("#capital").innerText = data[0].capital;
-    // document.querySelector("#language").innerText = data[0].languages;
+    document.querySelector("#language").innerText = Object.values(data[0].languages).join(", ");
     document.querySelector("#population").innerText = data[0].population;
 
-    // const languagesElement = document.querySelector("#language");
-    // const languagesString = data.map(language => language);
-    //     languagesElement.innerText = languagesString;
-    // const languages = data[0].languages;
-    // Object.keys(languages).forEach(languageCode) => (){
-    //     document.querySelector("#language").innerText = languages[languageCode];
-    // }
-   
+
+
+    
 }
 
-
-
 const button = document.querySelector("#btn1");
-button.addEventListener("click", () => { 
-    getCountryByName("Portugal");
-    console.log("button clicked");
+button.addEventListener("click", async () => {
+    const countryInput = document.querySelector("#countryInput");
+    const countryName = countryInput.value.trim();
+    
+     getCountryByName(countryName);
+    
 });
-
 
